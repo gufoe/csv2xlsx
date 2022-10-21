76ca705800718ac58591ba0c8da163399fcfeeef
Giacomo Rizzi <gufoes@gmail.com>
2022-10-21 16:03:36 +0200

    Upgrade all versions and migrate clap to structopt

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,32 @@
-use calamine::Error;
-use calamine::{open_workbook, Reader, Sheets};
-use clap::Parser;
+use calamine::open_workbook_auto;
+use calamine::Reader;
 use std::collections::HashMap;
-use std::path::Path;
+use structopt::StructOpt;
 use xlsxwriter::*;
 
-#[derive(Parser)]
-#[clap(version = "1.2", author = "Giacomo R. <gcmrzz@gmail.com>")]
+#[derive(StructOpt)]
+#[structopt(version = "1.2", author = "Giacomo R. <gcmrzz@gmail.com>")]
 struct Opts {
-    #[clap(short, long, help = "convert <input> to a csv file")]
+    #[structopt(short, long, help = "convert <input> to a csv file")]
     to_csv: bool,
-    #[clap(
+    #[structopt(
         short,
         long,
         help = "index of the sheet to be read if converting to csv",
         default_value = "0"
     )]
     sheet: usize,
-    #[clap(
-        short = 'n',
+    #[structopt(
+        short = "n",
         long,
         help = "sheet name to write (or read if converting to csv)"
     )]
     sheet_name: Option<String>,
-    #[clap(short, long)]
+    #[structopt(short, long)]
     input: String,
-    #[clap(short, long, default_value = "output.xlsx")]
+    #[structopt(short, long, default_value = "output.xlsx")]
     output: String,
-    #[clap(
+    #[structopt(
         short,
         long,
         help = "by default it depends on the content size - only useful if output is an excel file"
@@ -64,7 +63,7 @@ fn csv_to_excel(opts: Opts) {
             if value.len() > 0 {
                 let res = sheet.write_string((row_i) as u32, (col_i) as u16, value, None);
                 match res {
-                    Ok(data) => data, 
+                    Ok(data) => data,
                     Err(e) => {
                         panic!("{:?}", e.to_string())
                     }
@@ -88,34 +87,8 @@ fn csv_to_excel(opts: Opts) {
     workbook.close().unwrap();
 }
 
-fn open_workbook_xlsx<P: AsRef<Path>>(path: P) -> Result<Sheets, Error> {
-    Ok(Sheets::Xlsx(open_workbook(&path).map_err(Error::Xlsx)?))
-}
-fn open_workbook_xls<P: AsRef<Path>>(path: P) -> Result<Sheets, Error> {
-    Ok(Sheets::Xls(open_workbook(&path).map_err(Error::Xls)?))
-}
-fn open_workbook_xlsb<P: AsRef<Path>>(path: P) -> Result<Sheets, Error> {
-    Ok(Sheets::Xlsb(open_workbook(&path).map_err(Error::Xlsb)?))
-}
-fn open_workbook_ods<P: AsRef<Path>>(path: P) -> Result<Sheets, Error> {
-    Ok(Sheets::Ods(open_workbook(&path).map_err(Error::Ods)?))
-}
-fn open_auto<P: AsRef<Path>>(path: P) -> Result<Sheets, Error> {
-    if let Ok(ret) = open_workbook_xlsx(&path) {
-        Ok(ret)
-    } else if let Ok(ret) = open_workbook_xls(&path) {
-        Ok(ret)
-    } else if let Ok(ret) = open_workbook_xlsb(&path) {
-        Ok(ret)
-    } else if let Ok(ret) = open_workbook_ods(&path) {
-        Ok(ret)
-    } else {
-        Err(Error::Msg("Cannot detect file format"))
-    }
-}
-
 fn excel_to_csv(opts: Opts) {
-    let mut excel: Sheets = open_auto(&opts.input).unwrap();
+    let mut excel = open_workbook_auto(&opts.input).unwrap();
     let mut csv = csv::WriterBuilder::new()
         .flexible(true)
         .has_headers(false)
@@ -133,7 +106,7 @@ fn excel_to_csv(opts: Opts) {
 }
 
 fn _excel_to_csv_in_ram(opts: Opts) {
-    let mut excel: Sheets = open_auto(&opts.input).unwrap();
+    let mut excel = open_workbook_auto(&opts.input).unwrap();
     let range = if let Some(name) = opts.sheet_name {
         excel.worksheet_range(&name).unwrap().unwrap()
     } else {
@@ -154,7 +127,7 @@ fn _excel_to_csv_in_ram(opts: Opts) {
 }
 
 fn main() {
-    let opts: Opts = Opts::parse();
+    let opts: Opts = Opts::from_args();
 
     match opts.to_csv {
         true => excel_to_csv(opts),
